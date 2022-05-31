b2a03ee043f9bc68aa2f9e90bafb809e49fd7f97
meteor <tjdans34568@gmail.com>
2022-05-31 11:13:26 +0900

    start

--- src/bubble/test/ex01/BubbleFrame.java
+++ src/bubble/test/ex01/BubbleFrame.java
@@ -1,5 +1,19 @@
 package bubble.test.ex01;
 
-public class BubbleFrame {
+import javax.swing.JFrame;
+
+public class BubbleFrame extends JFrame {
+	
+	public BubbleFrame() {
+	
+		setSize(1000, 640);
+		setVisible(true); // 그림을 그려라
+		
+	}
+	
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+	
 
 }
